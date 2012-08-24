4e394267653a121a5e47c66ee3e6c9ea21d85684
Ilya <marchukilya@gmail.com>
2012-08-24 14:49:28 +0300

    Update src/Acme/AdminBoardboard/Controller/AdminBoardController.php

--- src/Acme/AdminBoardboard/Controller/AdminBoardController.php
+++ src/Acme/AdminBoardboard/Controller/AdminBoardController.php
@@ -3,6 +3,7 @@ namespace Acme\AdminBoard\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class AdminBoardController extends Controller
@@ -11,7 +12,7 @@ class AdminBoardController extends Controller
   {
 
  
-       return new Responce()'<html><body>Hello ' . $name. '!</body></html>';
+       return new Response()'<html><body>Hello ' . $name. '!</body></html>';
 
   }
 
