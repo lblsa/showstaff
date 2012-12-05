4802c98849e042a63da2162d6df2089f7b22d39d
vinilzen <marchukilya@gmail.com>
2012-12-05 15:33:08 +0100

    onePage ui for Order

--- web/js/order.js
+++ web/js/order.js
@@ -1,9 +1,17 @@
 //* update 2012-10-18 16:15:00 *//
-var	OrderModel, OrderCollection, 
-	ViewOrdersByRestaurant, ViewOrdersBySupplier, ViewOrders, ViewOrdersS, ViewSupplier, ViewRestaurant, 
-	products, restaurants, edit_mode, suppliers, orders, view_order_by_rest;
+var	OrderModel, OrderCollection, ViewOrdersByRestaurant, ViewOrdersBySupplier, 
+	ViewOrders, ViewOrdersS, ViewSupplier, ViewRestaurant, products, restaurants, 
+	edit_mode = 0, suppliers, orders, view_order_by_rest, Products, Restaurants, 
+	Suppliers, completed;
 
 $(function(){
+
+	$('#preloader').width($('#order_list').width());
+	$('#preloader').height($('#order_list').height());
+	var p = $('#order_list').position();
+	$('#preloader').css({'left':p.left, 'top': p.top});
+	$('#preloader').fadeIn('fast');
+
 	OrderModel = Backbone.Model.extend({ });
 
 	ViewOrdersByRestaurant = Backbone.View.extend({
@@ -21,13 +29,15 @@ $(function(){
 		},
 		
 		renderAll: function() {
-			
+
 			if (restaurants.length > 0) {
-				
+				$('#preloader').fadeOut();
 				$('.orders_by_rest').html('');
 				restaurants.each(function(model){
+
 					var view = new ViewRestaurant({model:model});
 					var content = view.render().el;
+
 					$('.orders_by_rest').append(content);
 					
 					// get orders for restaurant
@@ -48,7 +58,7 @@ $(function(){
 							$('td tbody', content).append(order_content);
 						});
 						
-						$('#preloader').fadeOut();
+						
 						
 					} else {
 						
@@ -204,7 +214,7 @@ $(function(){
 		
 		template: _.template(	'<td>'+
 									'<h4 class="pull-left"> <%= name %> <span class="edit_order">( '+
-										'<a href="/company/<%= company %>/restaurant/<%= id %>/order">править заказ ресторана</a>'+
+										'<a href="">править заказ ресторана</a>'+
 									' )</span></h4>'+
 								'</td>'),
 		
@@ -215,18 +225,80 @@ $(function(){
 		render: function(){
 			var content = this.template(this.model.toJSON());
 			this.$el.html(content);
-			if (edit_mode) {
-				if (href[href.length-1] != 'order') {
-					var link = $('.edit_order a', this.$el).attr('href')+'/'+href[href.length-1];
-					$('.edit_order a', this.$el).attr( 'href', link);
-				}
-			} else {
-				$('.edit_order', this.$el).remove();
-			}
+
+			$('.edit_order a', this.$el).attr('href', '/company/'+href[2]+'/restaurant/'+this.model.id+'/order/'+$('.wh_datepicker').val());
+
+			if (!edit_mode) $('.edit_order', this.$el).remove();
+
 			return this;
 		}
 	})
 
+	Products = Backbone.Collection.extend({
+		url: '/api/company/'+parseInt(href[2])+'/product',
+		parse: function(response, xhr){
+			if(response.code && (response.code == 200)){
+				return response.data;
+			} else {
+				error_fetch('bad product request');
+			}
+		}
+	})
+	Suppliers = Backbone.Collection.extend({
+		url: '/api/company/'+parseInt(href[2])+'/supplier',
+		parse: function(response, xhr){
+			if(response.code && (response.code == 200)){
+				return response.data;
+			} else {
+				error_fetch('bad supplier request');
+			}
+		}
+	})
+	Restaurants = Backbone.Collection.extend({
+		url: '/api/company/'+parseInt(href[2])+'/restaurant',
+		parse: function(response, xhr){
+			if(response.code && (response.code == 200)){
+				return response.data;
+			} else {
+				error_fetch('bad rest request');
+			}
+		}
+	})
+
+	OrderCollection = Backbone.Collection.extend({
+	  
+	  model: OrderModel,
+	  url: '/api/company/'+href[2]+'/order/'+$('.wh_datepicker').val(),
+	  parse: function(response, xhr){
+		if(response.code && (response.code == 200)){
+
+			if ('completed' in response) completed = response.completed;
+			if ('edit_mode' in response) edit_mode = response.edit_mode;
+
+			if ('completed_mode' in response && response.completed_mode == 1) {
+				$('.completed').fadeIn();
+
+				if (completed){
+					$('.order_close_msg').show();
+					$('.order_open_msg').hide();
+					$('.completed_yes').addClass('disabled');
+					$('.completed_no').removeClass('disabled');
+				} else {
+					$('.order_close_msg').hide();
+					$('.order_open_msg').show();
+					$('.completed_no').addClass('disabled');
+					$('.completed_yes').removeClass('disabled');	
+				}
+			} else {
+				$('.completed').fadeOut();
+			}
+
+			return response.data;
+		} else {
+			error_fetch('bad order request');
+		}
+	  }  
+	});
 
 	$(document).on('click', "#group_by_supp:not(.disabled)", function(){
 		$('.order_by_supp').remove();
@@ -255,7 +327,7 @@ $(function(){
 	$('.completed_yes').click(function(){
 		$.ajax({
 		  type: "PUT",
-		  url: href[4]?"/api/company/"+href[2]+"/order/"+href[4]:"/api/company/"+href[2]+"/order",
+		  url: "/api/company/"+href[2]+"/order/"+$('.wh_datepicker').val(),
 		  data: '{ "completed": 1 }',
 		  success: function(data) {
 		  	$('.completed .alert').remove();
@@ -282,7 +354,7 @@ $(function(){
 	$('.completed_no').click(function(){
 		$.ajax({
 		  type: "PUT",
-		  url: href[4]?"/api/company/"+href[2]+"/order/"+href[4]:"/api/company/"+href[2]+"/order",
+		  url: "/api/company/"+href[2]+"/order/"+$('.wh_datepicker').val(),
 		  data: '{ "completed": 0 }',
 		  success: function(data) {
 		  	$('.completed .alert').remove();
@@ -304,5 +376,148 @@ $(function(){
 		});
 		
 		return false;
-	})
+	});
+
+
+	$( "#smena_datapicker" ).datepicker({
+		onSelect: function(strDate, inst){	update(strDate); },
+		showOtherMonths: true,
+		selectOtherMonths: true,
+	});
+	
+	$( "#smena_datapicker" ).datepicker( "setDate", $('.wh_datepicker').val() );
+	
+	$('#prev_day, #next_day').click(function(){
+
+		var today = $("#smena_datapicker").datepicker("getDate");
+		var new_day = today;
+
+		if ($(this).attr('id') == 'next_day')
+			new_day.setDate(today.getDate() + 1);
+		
+		if ($(this).attr('id') == 'prev_day')
+			new_day.setDate(today.getDate() - 1);
+
+		$("#smena_datapicker").datepicker( "setDate", new_day );
+
+		var dd = new_day.getDate()<10?'0'+new_day.getDate():new_day.getDate();
+		var mm = new_day.getMonth()+1; //January is 0!
+		var yyyy = new_day.getFullYear();
+
+		update(yyyy+'-'+mm+'-'+dd);
+
+		return false;
+	});
+
+	products = new Products;
+	restaurants = new Restaurants;
+	suppliers = new Suppliers;
+
+	units.fetch({	
+		success: function(){
+
+			restaurants.fetch({	
+				success:function(collection, response){
+
+					products.fetch({
+						success:function(collection, response){
+
+							suppliers.fetch({
+								success:function(collection, response){
+
+									orders = new OrderCollection({}, {units: units, products: products}); // init collection
+
+
+									orders.fetch({	success:function(collection, response){
+
+														view_order_by_rest = new ViewOrdersByRestaurant({collection: collection}); // initialize view
+														$('#order_list').append(view_order_by_rest.render().el); // add tbody
+														view_order_by_rest.renderAll().el; // add inner
+
+													}, error: function(){
+															error_fetch('Ошибка при получении поставщиков');
+													}
+											});
+
+								}, error:function(){
+									error_fetch('Ошибка при получении поставщиков');
+								}
+							});
+
+						}, error:function(){
+							error_fetch('Ошибка при получении продуктов');
+						}
+					});
+
+				}, error:function(){
+					error_fetch('Ошибка при получении ресторанов');
+				}
+			});
+
+		}
+	});
+	
+	if (!edit_mode) $('.restaurant .edit_order').remove();
 })
+
+function update(strDate){
+		strDate = strDate;
+		$('.curent-date-header').html(strDate);
+		$('.wh_datepicker').val(strDate);
+
+		$('.download_excel').attr('href', '/company/'+href[2]+'/order/export/'+strDate);
+		
+		$( "#smena_datapicker" ).html('');
+		$( "#smena_datapicker" ).removeClass('hasDatepicker');
+
+		$( "#smena_datapicker" ).datepicker({
+			onSelect: function(strDate, inst){	update(strDate); },
+			showOtherMonths: true,
+			selectOtherMonths: true,
+		});
+		$( "#smena_datapicker" ).datepicker( "setDate", strDate );
+		
+		orders.url = '/api/company/'+href[2]+'/order/'+strDate;
+
+		document.title = $('.curent-page-title').text();
+		window.history.pushState({}, $('.curent-page-title').text(), '/company/'+href[2]+'/order/'+strDate);
+		
+		var today = new Date();
+		var dd = today.getDate()<10?'0'+today.getDate():today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear();
+
+		/*if ( yyyy+'-'+mm+'-'+dd < strDate)
+			$('.agreed_all').fadeIn();
+		else
+			$('.agreed_all').fadeOut();*/
+
+		$('#preloader').width($('#order_list').width());
+		$('#preloader').height($('#order_list').height());
+		var p = $('#order_list').position();
+		$('#preloader').css({'left':p.left, 'top': p.top});
+		$('#preloader').fadeIn('fast');
+
+		orders.fetch({	success: function(collection, response) {
+											
+											$('#group_by_rest').addClass('disabled');
+											$('#group_by_supp').removeClass('disabled');
+
+											view_order_by_rest.remove();
+											$('.orders_by_rest').remove();
+											$('.order_by_supp').remove();
+											
+											view_order_by_rest = new ViewOrdersByRestaurant({collection: collection}); // initialize view
+											$('#order_list').append(view_order_by_rest.render().el); // add template
+											view_order_by_rest.renderAll().el;
+											/*view_workinghours.remove();
+											$('.workinghours').remove();
+											view_workinghours = new ViewWorkinghours({collection: collection});
+											$('#shift_list').append(view_workinghours.render().el);
+											view_workinghours.renderAll().el; */
+										}, 
+										error: function(){
+											error_fetch('Ошибка при получении консолидированного заказа. Обновите страницу или обратитесь к администратору');
+										}
+							});
+}
